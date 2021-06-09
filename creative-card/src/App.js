6b2a4eab9eb79b9fcebf8f7439cd4c5fe2b4311e
anjaf <anja.feuerhuber@students.fhnw.ch>
2021-06-09 16:29:01 +0200

    first step react-app

---
+++ creative-card/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+          Creative Card
+          Order a special card.
+      </header>
+    </div>
+  );
+}
+
+export default App;
